c117f932dcf7a9eaef169e66b3b84b4ba204c581
renathossain <renathossain@gmail.com>
2024-07-12 11:25:25 -0400

    Counter Implemented

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+    let counter1 = 0;
+    let counter2 = 0;
+</script>
+
+<div class="container">
+    <div class="counter">{counter1}</div>
+    <button class="increment" on:click={() => (counter1 += 1)}
+        >Increment Counter 1</button
+    >
+</div>
+<div class="container">
+    <div class="counter">{counter2}</div>
+    <button class="increment" on:click={() => (counter2 += 1)}
+        >Increment Counter 2</button
+    >
+</div>
+
+<style>
+    .container {
+        display: inline-block;
+        padding: 20px;
+        margin: 20px;
+        border: 2px solid black;
+        text-align: center;
+    }
+
+    .counter {
+        font-size: 200px;
+    }
+
+    .increment {
+        font-size: 20px;
+    }
+</style>
